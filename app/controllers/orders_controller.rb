919f13fa11bde1a2e667359e949bff0a9bc97f21
Deathis <p.puchalski@yahoo.pl>
2014-08-26 21:01:02 +0200

    incomplete cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,15 @@ class OrdersController < ApplicationController
   end
 
   def put_to_cart
-    #current_order.line_items
-    redirect_to :cart
+    quantity = 1 unless params[:quantity]
+    populator = Spree::OrderPopulator.new(current_order(create_order_if_necessary: true), current_currency)
+
+    if populator.populate(params[:variant_id], quantity, params[:options])
+      redirect_to :cart
+    else
+      flash[:error] = populator.errors.full_messages.join(" ")
+      redirect_back_or_default(spree.root_path)
+    end
   end
 
   def remove_from_cart
@@ -16,12 +23,35 @@ class OrdersController < ApplicationController
   end
 
   private
-    def current_order
-      Spree::Order.incomplete.first || Order.create(order_params)
+    def current_order(options = {})
+      options[:create_order_if_necessary] ||= false
+      options[:lock] ||= false
+
+      return @current_order if @current_order
+
+      # Find any incomplete orders for the guest_token
+      @current_order = Spree::Order.incomplete.includes(:adjustments).lock(options[:lock]).find_by(current_order_params)
+
+      if options[:create_order_if_necessary] and (@current_order.nil? or @current_order.completed?)
+        @current_order = Spree::Order.new(current_order_params)
+        @current_order.user ||= try_spree_current_user
+        # See issue #3346 for reasons why this line is here
+        @current_order.created_by ||= try_spree_current_user
+        @current_order.save!
+      end
+
+      if @current_order
+        #@current_order.last_ip_address = ip_address
+        return @current_order
+      end
     end
 
-    def order_params
-      params[:created_by_id] = try_spree_current_user.try(:id)
+    def current_order_params
+      params[:created_by_id] = current_user.id
       params.permit(:created_by_id)
+    end
+
+    def current_currency
+      Spree::Config[:currency]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/cart', :to => 'orders#cart', as: :cart
-  get '/cart/add-product/:id', :to => 'orders#put_to_cart', as: :put_to_cart
-  get '/cart/remove-product/:id', :to => 'orders#remove_from_cart', as: :remove_from_cart
+  get '/cart/add-product/:variant_id', :to => 'orders#put_to_cart', as: :put_to_cart
+  get '/cart/remove-product/:variant_id', :to => 'orders#remove_from_cart', as: :remove_from_cart
   get '/cart', :to => 'orders#change_quantity', as: :change_quantity_cart
   get '/checkout', to: 'orders#checkout', as: :checkout
 
