1ef15940e70670453a8e84e8a2eec6ed28b724c1
Miłosz Osiński <mylosz@yahoo.pl>
2014-10-02 17:53:24 +0000

    add cart alert

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,28 +1,11 @@
-/*
-var product_to_delete = null;
-
 $(document).on("page:change", function() {
-  if (($('form#update-cart')).is('*')) {
-    ($('form#update-cart a.delete')).show().one('click', function() {
-      (product_to_delete = ($(this)).parents('.line-item').first()).find('input.cart_quantity_input').val(0);
-      ($(this)).parents('form').first().submit();
-      return false;
-    });
-  }
-  return ($('form#update-cart')).submit(function() {
-    return ($('form#update-cart #update-button')).attr('disabled', true);
+  $("form.edit_order").on('change', 'input', function() {
+    $("div#notice").html("<div class='alert alert-warning'><strong>Attention!</strong> Unsaved changes. Please click the update button.</div>");
   });
-
-});
-
-$(document).on("ajax:success", "form#update-cart", function(e, data, status, xhr) {
-  if (!data.success) {
-    product_to_delete.hide();
-    product_to_delete = null
-  }
-});
-*/
-$(document).on("page:change", function() {
-  $("a.cart_quantity_up").click(function() { $(this).next("input.cart_quantity_input").get(0).value++; });
-  $("a.cart_quantity_down").click(function() { $(this).prev("input.cart_quantity_input").get(0).value--; });
+  $("a.cart_quantity_up").click(function() {
+    $(this).next("input.cart_quantity_input").trigger('change').get(0).value++;
+  });
+  $("a.cart_quantity_down").click(function() {
+    $(this).prev("input.cart_quantity_input").trigger('change').get(0).value--;
+  });
 });
