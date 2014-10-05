59908b2f1a07880f943dd473f6fcdb2c0277495c
Miłosz Osiński <mylosz@yahoo.pl>
2014-10-05 23:42:39 +0000

    add product availability to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Spree::Product.all.search(params[:search]).select{|product| product.available?&product.images.present?&(product.total_on_hand > 0)}
+    @products = Spree::Product.all.search(params[:search]).select{|product| product.images.present?&(product.total_on_hand > 0)}
     @products = @products.select{|product| product.price >= params[:price_min].to_i} if params[:price_min].present?
     @products = @products.select{|product| product.price <= params[:price_max].to_i} if params[:price_max].present?
     @products = @products.paginate(page: params[:page], per_page: 9)
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,14 @@
 Spree::Product.class_eval do
+  include ActionView::Helpers::TagHelper
+
   def self.search(search)
     order('created_at DESC').where('name LIKE ?', "%#{search}%")
+  end
+
+  def availability
+    case
+      when self.available? then content_tag(:strong, "In Stock", :class => "text-success")
+      when !self.available? then content_tag(:strong, "Out Of Stock", :class => "text-danger")
+    end
   end
 end
