d241911d163d19903639b8a08253becea4c32095
Miłosz Osiński <mylosz@yahoo.pl>
2014-10-14 20:37:11 +0000

    correct price range functionality & add reset button

--- app/assets/javascripts/price-range.js
+++ app/assets/javascripts/price-range.js
@@ -1,15 +1,17 @@
-var ready;
-ready = function() {
-  $( "#slider-range" ).slider({
+var ready = function() {
+  $("#slider-range").slider({
     range: true,
     min: 0,
     max: 600,
-    values: [ $("#price_min").val() || 75, $("#price_max").val() || 400 ],
+    values: [ $(".price-range #price_min").val() || 75, $(".price-range #price_max").val() || 400 ],
     slide: function( event, ui ) {
-      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      $( "#price_min" ).val( ui.values[ 0 ] );
-      $( "#price_max" ).val( ui.values[ 1 ] );
+      $("#amount").val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      $(".price-range #price_min").val( ui.values[ 0 ] );
+      $(".price-range #price_max").val( ui.values[ 1 ] );
     }
+  });
+  $("#price-reset").click(function() {
+    $(".price-range #price_min, .price-range #price_max").val('');
   });
 };
 
