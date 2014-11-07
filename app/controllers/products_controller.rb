2eeefdcd31ae3d179fdda3292273c306046441a8
Miłosz Osiński <mylosz@yahoo.pl>
2014-11-07 13:09:17 +0000

    first part of sidebar categories tree

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,10 @@ class ProductsController < ApplicationController
     @products = @products.select{|product| product.price >= params[:price_min].to_i} if params[:price_min].present?
     @products = @products.select{|product| product.price <= params[:price_max].to_i} if params[:price_max].present?
     @products = @products.paginate(page: params[:page], per_page: 9)
+    @category = Spree::Taxon.find(params[:category]) if params[:category]
+    if @category
+      @category = @category.parent if !@category.children.present?
+    end
   end
 
   def show
