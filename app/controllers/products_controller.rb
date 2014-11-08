8e17f16eb15463ccbc8a9bb48f9aba3cc566ee8c
Miłosz Osiński <mylosz@yahoo.pl>
2014-11-08 14:15:55 +0000

    fix category products filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,9 @@ class ProductsController < ApplicationController
     @products = Spree::Product.all.search(params[:search]).select{|product| product.images.present?&&(product.total_on_hand > 0)}
     @products = @products.select{|product| product.price >= params[:price_min].to_i} if params[:price_min].present?
     @products = @products.select{|product| product.price <= params[:price_max].to_i} if params[:price_max].present?
+    @products = @products.select{|product| product.taxons.map(&:id).include?(params[:category].to_i)} if params[:category].present?
     @products = @products.paginate(page: params[:page], per_page: 9)
-    @category = Spree::Taxon.where(id: params[:category]) if params[:category]
+    @category = Spree::Taxon.where(id: params[:category])
     unless @category.empty?
       @category = [@category.first.parent] if !@category.first.children.present?
     end
