d604c847052a818f21b310ff58d8fd045157d4ec
Miłosz Osiński <mylosz@yahoo.pl>
2014-11-08 17:14:47 +0000

    resolve conflicts category tree with search and price range

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,13 @@ class ProductsController < ApplicationController
     @products = @products.select{|product| product.price <= params[:price_max].to_i} if params[:price_max].present?
     @products = @products.select{|product| product.taxons.map(&:id).include?(params[:category].to_i)} if params[:category].present?
     @products = @products.paginate(page: params[:page], per_page: 9)
-    @category = Spree::Taxon.where(id: params[:category])
-    unless @category.empty?
-      @category = [@category.first.parent] if !@category.first.children.present?
+    @category = Spree::Taxon.where(id: params[:category]) if params[:category].present?
+    if @category
+      unless @category.empty?
+        @category = [@category.first.parent] if !@category.first.children.present?
+      else
+        @category = nil
+      end
     end
   end
 
