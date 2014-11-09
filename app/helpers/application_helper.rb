a01e0a4de1c38d099bbe1e8f3d5200cfd4a564e9
Miłosz Osiński <mylosz@yahoo.pl>
2014-11-09 10:37:25 +0000

    active select sidebar categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def current_category(category, category2)
+    content_tag(:li) do
+      if category.id == category2.to_i
+        link_to("#{category.name} (#{category.products.count})", products_path(params.merge({category: category.id})), class: 'active')
+      else
+        link_to("#{category.name} (#{category.products.count})", products_path(params.merge({category: category.id})))
+      end
+    end
+  end
 end
